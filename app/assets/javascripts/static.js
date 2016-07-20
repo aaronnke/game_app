178e0293824cf9e814ec67fd326e2862ae46f363
KevinSia <kevinsiakeemun@gmail.com>
2016-07-20 10:44:52 +0800

    added files

--- app/assets/javascripts/static.js
+++ app/assets/javascripts/static.js
@@ -1,89 +1,110 @@
+var snake_move, paused;
+
 $(document).ready(function() {
 
-  $(document).keydown(function(key) {
-    switch(parseInt(key.which,10)) {
-			case 37:
-				direction = "left";
-				break;
-			case 38:
-				direction = "up";
-				break;
-			case 39:
-				direction = "right";
-				break;
-			case 40:
-				direction = "down";
-				break;
-		}
-	});
-
-
-  // function move() {
-  // 	var lastLeft = $("#snake-head").position().left
-  // 	var lastTop = $("#snake-head").position().top
-
-  // 	if (direction === "left") {
-  // 		$('#snake-head').css("left", "-=2vh");
-  // 	}
-  // 	else if (direction === "up") {
-  // 		$('#snake-head').css("top", "-=2vh");
-  // 	}
-  // 	else if (direction === "right") {
-  // 		$('#snake-head').css("left", "+=2vh");
-  // 	}
-  // 	else if (direction === "down") {
-  // 		$('#snake-head').css("top", "+=2vh");
-  // 	}
-  // 	$('#snake-tail').css("left", lastLeft);
-  // 	$('#snake-tail').css("top", lastTop);
-
-  // 	document.getElementById("snake-tail").id = 'snake-body-1'
-  // 	document.getElementById("snake-body-1").id = 'snake-tail'
-  // }
-
-  function move() {
-  	var headLastLeft = $("#snake-head").position().left;
-  	var headLastTop = $("#snake-head").position().top;
-  	var bodyLastLeft = $("#snake-body-1").position().left;
-  	var bodyLastTop = $("#snake-body-1").position().top;
-
-  	if (direction === "left") {
-  		$('#snake-head').css("left", "-=2vh");
-  	}
-  	else if (direction === "up") {
-  		$('#snake-head').css("top", "-=2vh");
-  	}
-  	else if (direction === "right") {
-  		$('#snake-head').css("left", "+=2vh");
-  	}
-  	else if (direction === "down") {
-  		$('#snake-head').css("top", "+=2vh");
-  	}
-  	$('#snake-body-1').css("left", headLastLeft);
-  	$('#snake-body-1').css("top", headLastTop);
-  	$('#snake-body-2').css("left", bodyLastLeft);
-  	$('#snake-body-2').css("top", bodyLastTop);
-
-  }
-
-
+  var direction, stop, game_started;
+  var default_pos = 180;
+  var body_count = 2;
+  var game_box_obj = $('#game-box')[0].getBoundingClientRect();
   var play = false;
 
+  function reset_pos() {
+      movement = { top: default_pos, left: default_pos };
+      $.each($('.snake').get().reverse(), function(index, el){
+        $(el).css(movement);
+      });
+  }
+
+  function start_game() {
+    if (!play) {
+      var rand_color = ["#c5e1a5", "#ffe082", "#4fc3f7", "#f48fb1", "#ef9a9a", "#ce93d8", "#c5cae9", "#80cbc4", "#ffab91", "#fff59d"][Math.floor(Math.random() * 10)];
+      $(".snake").css("background-color", rand_color);
+      reset_pos();
+      start_snake_move();
+      play = true;
+    }
+  }
+
+  function stop_game() {
+    clearInterval(snake_move);
+    play = false;
+  }
+
+  function pause_game() {
+    clearInterval(snake_move);
+  }
+
+  function start_snake_move() {
+    snake_move = setInterval(move_snake, 100);
+  }
+
+  // add position
+  // if position exceed limit, dont apply and stop
+  // else apply all css
+  function move_snake() {
+    var headCurrentLeft, headCurrentTop, bodyTop, bodyLeft;
+    var headLastLeft = $("#snake-head").position().left;
+    var headLastTop = $("#snake-head").position().top;
+
+    if (direction === "left") {
+      headCurrentLeft = headLastLeft - 15;
+    }
+    else if (direction === "up") {
+      headCurrentTop = headLastTop - 15;
+    }
+    else if (direction === "right") {
+      headCurrentLeft = headLastLeft + 15;
+    }
+    else if (direction === "down") {
+      headCurrentTop = headLastTop + 15;
+    }
+
+    if(headCurrentTop >= 375 || headCurrentTop <= -15 || headCurrentLeft <= -15 || headCurrentLeft >= 375){
+      stop_game();
+      alert('Game Over');
+      reset_pos();
+    }
+    else{
+      $("#snake-head").css({top: headCurrentTop, left: headCurrentLeft});
+      $.each($('.snake-body'), function(index, el){
+        bodyTop = $(el).position().top;
+        bodyLeft = $(el).position().left;
+        $(el).css({top: headLastTop, left: headLastLeft});
+        headLastTop = bodyTop;
+        headLastLeft = bodyLeft;
+      });
+    }
+  }
+
+  $(document).keydown(function(key) {
+    switch(parseInt(key.which, 10)) {
+      case 37:
+        if(direction != 'right'){ direction = "left" };
+        break;
+      case 38:
+        if(direction != 'down'){ direction = "up"; }
+        break;
+      case 39:
+        if(direction != 'left'){ direction = "right"; }
+        break;
+      case 40:
+        if(direction != 'up'){ direction = "down"; }
+        break;
+    }
+  });
+
   $('#start-game').click(function() {
-  	if (play === false) {
-	  	var rand_color = ["#c5e1a5", "#ffe082", "#4fc3f7", "#f48fb1", "#ef9a9a", "#ce93d8", "#c5cae9", "#80cbc4", "#ffab91", "#fff59d"][Math.floor(Math.random() * 10)];
-	  	$(".snake").css("background-color", rand_color)
-		  gameStart = setInterval(move, 200);
-		}
-		play = true;
-  })
+    start_game();
+  });
 
   $('#pause-game').click(function() {
-  	clearInterval(gameStart);
-  	play = false;
-  })
-
-
-
+    if($(this).attr('clicked')){
+      paused = false;
+      start_snake_move();
+    }
+    else {
+      paused = true;
+      pause_game();
+    }
+  });
 });
-
